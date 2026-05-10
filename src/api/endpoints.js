83169b9ece36dd1ee068b19dc0ab5483ebe558cf
Prateek Chitransh <prateekchitransh@gmail.com>
2026-05-10 14:29:20 +0530

    created a store and added endpoints

---
+++ src/api/endpoints.js
@@ -0,0 +1,15 @@
+import status from "daisyui/components/status";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/api';
+
+export const ENDPOINTS = {
+    AUTH_LOGIN: `${API_BASE_URL}/login`,
+    AUTH_SIGNUP: `${API_BASE_URL}/signup`,
+    PROFILE_VIEW: `${API_BASE_URL}/profile/view`,
+    PROFILE_EDIT: `${API_BASE_URL}/profile/edit`,
+    FEED: `${API_BASE_URL}/user/feed`,
+    CONNECTIONS: `${API_BASE_URL}/user/connections`,
+    REQUESTS_PENDING: `${API_BASE_URL}/user/requests/pending`,
+    REQUEST_SEND: (status, userId) => `${API_BASE_URL}/request/send/${status}/${userId}`,
+    REQUEST_REVIEW: (status, requestId) => `${API_BASE_URL}/request/review/${status}/${requestId}`,
+}
