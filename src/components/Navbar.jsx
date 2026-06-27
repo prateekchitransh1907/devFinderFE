0055573c52200095a79533f5878acc4013cddfb2
Prateek Chitransh <prateekchitransh@gmail.com>
2026-06-28 00:16:43 +0530

    menu z-index issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,7 +68,7 @@ function Navbar() {
                     </div>
                     <ul
                         tabIndex={-1}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow border border-grey-200 rounded-lg"
                     >
                         <li>
                             <Link to="/profile" className="justify-between">
